1d81738d08b2bf62ebccc46e783447578a129190
Louis <yienlouis470@gmail.com>
2024-12-04 00:13:32 +0300

    complete assignment

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,78 @@
 class Song:
-    pass
+    count = 0
+    genres = []
+    artists = []
+    genre_count = {}
+    artist_count = {}
+
+    def __init__(self, name, artist, genre):
+        self.name = name
+        self.artist = artist
+        self.genre = genre
+
+        self.add_song_to_count()
+
+        self.add_to_genres(genre)
+        self.add_to_artists(artist)
+
+        self.add_to_genre_count(genre)
+        self.add_to_artist_count(artist)
+
+    @classmethod
+    def add_song_to_count(cls):
+        cls.count += 1
+
+    @classmethod
+    def add_to_genres(cls, genre):
+        if genre not in cls.genres:
+            cls.genres.append(genre)
+
+    @classmethod
+    def add_to_artists(cls, artist):
+        if artist not in cls.artists:
+            cls.artists.append(artist)
+
+    @classmethod
+    def add_to_genre_count(cls, genre):
+        if genre in cls.genre_count:
+            cls.genre_count[genre] += 1
+        else:
+            cls.genre_count[genre] = 1
+
+    @classmethod
+    def add_to_artist_count(cls, artist):
+        if artist in cls.artist_count:
+            cls.artist_count[artist] += 1
+        else:
+            cls.artist_count[artist] = 1
+
+    @classmethod
+    def get_song_count(cls):
+        return cls.count
+
+    @classmethod
+    def get_all_genres(cls):
+        return cls.genres
+
+    @classmethod
+    def get_all_artists(cls):
+        return cls.artists
+
+    @classmethod
+    def get_genre_count(cls):
+        return cls.genre_count
+
+    @classmethod
+    def get_artist_count(cls):
+        return cls.artist_count
+
+song1 = Song("99 Problems", "Jay-Z", "Rap")
+song2 = Song("Halo", "Beyonce", "Pop")
+song3 = Song("God's Plan", "Drake", "Rap")
+
+print(Song.get_song_count())   
+print(Song.get_all_genres())         
+print(Song.get_all_artists())        
+print(Song.get_genre_count())        
+print(Song.get_artist_count())       
+
